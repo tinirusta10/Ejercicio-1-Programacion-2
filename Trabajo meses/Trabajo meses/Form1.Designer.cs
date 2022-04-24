2096cb1e61318daf431120e59bd0b71567f2295f
tinirusta10 <88777631+tinirusta10@users.noreply.github.com>
2022-04-24 15:36:00 -0300

    Add files via upload

---
+++ Trabajo meses/Trabajo meses/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Trabajo_meses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbldia = new System.Windows.Forms.Label();
+            this.lblmes = new System.Windows.Forms.Label();
+            this.lblaño = new System.Windows.Forms.Label();
+            this.lbltexto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(75, 45);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 0;
+            this.txt1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(75, 92);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 1;
+            this.txt2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt2_KeyPress);
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(75, 138);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 2;
+            this.txt3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt3_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(75, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Dia siguiente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbldia
+            // 
+            this.lbldia.AutoSize = true;
+            this.lbldia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldia.Location = new System.Drawing.Point(349, 92);
+            this.lbldia.Name = "lbldia";
+            this.lbldia.Size = new System.Drawing.Size(0, 17);
+            this.lbldia.TabIndex = 4;
+            // 
+            // lblmes
+            // 
+            this.lblmes.AutoSize = true;
+            this.lblmes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmes.Location = new System.Drawing.Point(417, 92);
+            this.lblmes.Name = "lblmes";
+            this.lblmes.Size = new System.Drawing.Size(0, 17);
+            this.lblmes.TabIndex = 5;
+            // 
+            // lblaño
+            // 
+            this.lblaño.AutoSize = true;
+            this.lblaño.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblaño.Location = new System.Drawing.Point(501, 92);
+            this.lblaño.Name = "lblaño";
+            this.lblaño.Size = new System.Drawing.Size(0, 17);
+            this.lblaño.TabIndex = 6;
+            this.lblaño.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbltexto
+            // 
+            this.lbltexto.AutoSize = true;
+            this.lbltexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltexto.Location = new System.Drawing.Point(381, 63);
+            this.lbltexto.Name = "lbltexto";
+            this.lbltexto.Size = new System.Drawing.Size(0, 17);
+            this.lbltexto.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Dia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Mes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Año";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(2, -1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(301, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ingrese la fecha en formato dd/mm/aaaa";
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(445, 216);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 12;
+            this.btnsalir.Text = "salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 274);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbltexto);
+            this.Controls.Add(this.lblaño);
+            this.Controls.Add(this.lblmes);
+            this.Controls.Add(this.lbldia);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbldia;
+        private System.Windows.Forms.Label lblmes;
+        private System.Windows.Forms.Label lblaño;
+        private System.Windows.Forms.Label lbltexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
---
+++ Trabajo meses/Trabajo meses/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo_meses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int diaacum = Convert.ToInt32(txt1.Text);
+            int mesacum = Convert.ToInt32(txt2.Text);          //variables a usar//
+            int añoacum = Convert.ToInt32(txt3.Text);
+            string eleccion = txt2.Text;
+
+            int dia = Convert.ToInt32(txt1.Text);
+            int mes = Convert.ToInt32(txt2.Text);
+            int año = Convert.ToInt32(txt3.Text);
+
+            if (dia < 31)
+            {
+                diaacum = dia + 1;
+            }
+
+          
+
+
+
+
+            switch (mesacum)                            //Escribe los meses en letras//
+            {
+                case 01:
+                    lblmes.Text = "enero del:";
+                    break;
+                case 02:
+
+                    if (añoacum % 4 == 0 && diaacum == 28)
+                    {
+                        lblmes.Text = "Febrero del";
+                        diaacum = 29;
+                    }
+                    if ( añoacum % 400 == 0 && diaacum == 28)        //valida año bisiesto//
+                    {
+                        lblmes.Text = "febrero del";
+                        diaacum = 29;
+
+                    }
+                    if ( diaacum < 28)
+                    {
+                        lblmes.Text = "frbero del";
+                    }
+
+
+                    if (diaacum > 27 && añoacum % 4 != 0)
+                    {
+                        lblmes.Text = "Marzo del ";
+                        diaacum = 1;
+                    }
+                 
+
+
+                    break;
+                case 03:
+
+                    if (diaacum >=31)
+                    {
+                        lblmes.Text = "Abril del :";
+                        diaacum = 1;
+                    
+                    }
+                    else if (diaacum <31)
+                    {
+                        lblmes.Text = "Marzo del";
+                    }
+                    
+                   
+                    break;
+                
+                
+                case 04:
+
+                    if (diaacum >= 30)
+                    {
+                        lblmes.Text = "Mayo del";
+                        diaacum = 1;
+
+                    }
+                    else if (mesacum < 30)
+                    {
+                        lblmes.Text = "abril del";
+
+                    }
+                    break;
+                case 05:
+                    if (diaacum >= 31)
+                    {
+                        lblmes.Text = "Junio del";
+                        diaacum = 1;
+
+                    }
+                    else if (diaacum < 31)
+                    {
+                        lblmes.Text = "mayo del";
+                    }
+
+
+                   
+                    break;
+                case 06:
+
+                    if (diaacum >= 30)
+                    {
+                        lblmes.Text = "Julio del";
+                        diaacum = 1;
+
+                    }
+                    else if (diaacum < 30)
+                    {
+                        lblmes.Text = "junio del";
+                    }
+
+                    break;
+                case 07:
+
+                    if (diaacum >= 31)
+                    {
+                        lblmes.Text = "Agosto del";
+                        diaacum = 1;
+
+                    }
+                    else if (diaacum < 31)
+                    {
+                        lblmes.Text = "Julio del";
+                    }
+                   
+                    break;
+                case 08:
+
+                    if (diaacum >= 31)
+                    {
+                        lblmes.Text = "Septiembre del";
+                        diaacum = 1;
+
+                    }
+                    else if (diaacum < 31)
+                    {
+                        lblmes.Text = "Agosto del";
+                    }
+                   
+                    break;
+                case 09:
+
+                    if (diaacum >= 30)
+                    {
+                        lblmes.Text = "Septiembre del";
+                        diaacum = 1;
+
+                    }
+                    lblmes.Text = "Octubre del";
+                    break;
+                case 10:
+
+                    if (diaacum >= 31)
+                    {
+                        lblmes.Text = "Noviembre del";
+                        diaacum = 1;
+
+                    }
+                    else if (diaacum < 31)
+                    {
+                        lblmes.Text = "Octubre del";
+                    }                    
+                    break;
+                case 11:
+                    if (diaacum >= 30)
+                    {
+                        lblmes.Text = "Diciembre del";
+                        diaacum = 1;
+
+                    }
+   
+                    else if (diaacum < 30)
+                    {
+                        lblmes.Text = "Nobiembre del";
+                    }
+
+                    break;
+                case 12:
+                    if (diaacum < 31)
+                    {
+                        lblmes.Text = "Diciembre del";
+                    }
+                    if (diaacum >= 31)
+                    {
+                        lblmes.Text = "Enero del";
+                        diaacum = 1;
+                        añoacum = año + 1;
+                    }
+
+                    break;
+
+                default:
+                    break;
+
+            }
+
+            
+            lbltexto.Text = "El siguiente dia es";
+            lbldia.Text = Convert.ToString(diaacum) + "     " + "de";
+            lblaño.Text = Convert.ToString(añoacum);                        //muestra el año y dia//
+            txt1.Clear();
+            txt2.Clear();
+            txt3.Clear();
+
+
+        }
+
+       
+        
+        
+        
+        
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo números", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txt2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo números", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txt3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo números", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var eleccion = DialogResult;
+            for (int i = 0; i < 1; i++)                                     /*esta estrutura repetitiva muestra un mensaje al apretar el boton salir*/
+            {
+                eleccion = MessageBox.Show("¿Desea salir?", "Salir", MessageBoxButtons.YesNo);
+
+                if (eleccion == DialogResult.No)
+                {
+                    break;
+                }
+            }
+
+            if (eleccion == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
